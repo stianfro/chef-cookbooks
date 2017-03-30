95bba99e024f557231a028716435283790b66618
stianfro <stianfroy@gmail.com>
2017-03-30 21:40:43 +0200

    Plexwatch (#8)
    * initial commit plexwatch + rubocop
    
    * packages, config & templates
    
    * kitchen logs
    
    * fixed crontab
    
    * fixed secret
    
    * added comma

---
+++ plexwatch/recipes/config.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook:: plexwatch
+# Recipe:: config
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+directory '/opt/plexWatch' do
+  mode '0777'
+  action :create
+end
+
+template '/opt/plexWatch/plexWatch.pl' do
+  source 'plexWatch.erb'
+  mode '0755'
+  action :create
+end
+
+template '/opt/plexWatch/config.pl' do
+  source 'config.erb'
+  action :create
+  variables(
+    provider_key: node['provider_key']['pushbullet']
+  )
+end
+
+cron 'plexWatch' do
+  command '/opt/plexWatch/config.pl --recently_added=movie,tv'
+end
